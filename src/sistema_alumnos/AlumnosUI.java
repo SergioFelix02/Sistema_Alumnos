de705bc68ccea254550127e0f358b5244e14ed52
Sergio Felix <Sergio Felix@RyzenPC>
2021-11-18 22:29:14 -0700

    Nombres de Variables y Comentarios

--- src/sistema_alumnos/AlumnosUI.java
+++ src/sistema_alumnos/AlumnosUI.java
@@ -11,170 +11,174 @@ public class AlumnosUI extends javax.swing.JFrame {
     public AlumnosUI() {
         initComponents();
     }
-    // <editor-fold defaultstate="collapsed" desc="Generated
-    // Code">//GEN-BEGIN:initComponents
+    
+    // Inicializar Componentes
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenu3 = new javax.swing.JMenu();
-        jMenuItem7 = new javax.swing.JMenuItem();
+        JMenuBar = new javax.swing.JMenuBar();
+        MenuAlumnos = new javax.swing.JMenu();
+        MI_Insertar = new javax.swing.JMenuItem();
+        MI_Modificar = new javax.swing.JMenuItem();
+        MI_Desactivar = new javax.swing.JMenuItem();
+        MI_Activos = new javax.swing.JMenuItem();
+        MI_Todos = new javax.swing.JMenuItem();
+        MenuCampus = new javax.swing.JMenu();
+        MI_InsertarC = new javax.swing.JMenuItem();
+        MenuSalir = new javax.swing.JMenu();
+        MI_Cerrar = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema Alumnos");
         setResizable(false);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(new Object[][] { {} }, new String[] {
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {}
+            },
+            new String [] {
 
-        }));
+            }
+        ));
         jScrollPane1.setViewportView(jTable1);
 
-        jMenu1.setText("Alumnos");
+        MenuAlumnos.setText("Alumnos");
 
-        jMenuItem1.setText("Insertar");
-        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+        MI_Insertar.setText("Insertar");
+        MI_Insertar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem1ActionPerformed(evt);
+                MI_InsertarActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem1);
+        MenuAlumnos.add(MI_Insertar);
 
-        jMenuItem2.setText("Modificar");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+        MI_Modificar.setText("Modificar");
+        MI_Modificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
+                MI_ModificarActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem2);
+        MenuAlumnos.add(MI_Modificar);
 
-        jMenuItem3.setText("Desactivar");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+        MI_Desactivar.setText("Desactivar");
+        MI_Desactivar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
+                MI_DesactivarActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem3);
+        MenuAlumnos.add(MI_Desactivar);
 
-        jMenuItem4.setText("Mostrar Activos");
-        jMenu1.add(jMenuItem4);
-
-        jMenuItem5.setText("Mostrar Todos");
-        jMenu1.add(jMenuItem5);
-
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Campus");
-
-        jMenuItem6.setText("Insertar");
-        jMenuItem6.addActionListener(new java.awt.event.ActionListener() {
+        MI_Activos.setText("Mostrar Activos");
+        MI_Activos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem6ActionPerformed(evt);
+                MI_ActivosActionPerformed(evt);
             }
         });
-        jMenu2.add(jMenuItem6);
+        MenuAlumnos.add(MI_Activos);
 
-        jMenuBar1.add(jMenu2);
-
-        jMenu3.setText("Salir");
-        jMenu3.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jMenu3MouseClicked(evt);
-            }
-        });
-        jMenu3.addActionListener(new java.awt.event.ActionListener() {
+        MI_Todos.setText("Mostrar Todos");
+        MI_Todos.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenu3ActionPerformed(evt);
+                MI_TodosActionPerformed(evt);
             }
         });
+        MenuAlumnos.add(MI_Todos);
 
-        jMenuItem7.setText("Cerrar Sesion");
-        jMenuItem7.addActionListener(new java.awt.event.ActionListener() {
+        JMenuBar.add(MenuAlumnos);
+
+        MenuCampus.setText("Campus");
+
+        MI_InsertarC.setText("Insertar");
+        MI_InsertarC.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem7ActionPerformed(evt);
+                MI_InsertarCActionPerformed(evt);
             }
         });
-        jMenu3.add(jMenuItem7);
+        MenuCampus.add(MI_InsertarC);
 
-        jMenuBar1.add(jMenu3);
+        JMenuBar.add(MenuCampus);
 
-        setJMenuBar(jMenuBar1);
+        MenuSalir.setText("Salir");
+
+        MI_Cerrar.setText("Cerrar Sesion");
+        MI_Cerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MI_CerrarActionPerformed(evt);
+            }
+        });
+        MenuSalir.add(MI_Cerrar);
+
+        JMenuBar.add(MenuSalir);
+
+        setJMenuBar(JMenuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup().addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
-                        .addContainerGap()));
-        layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING).addGroup(
-                javax.swing.GroupLayout.Alignment.TRAILING,
-                layout.createSequentialGroup().addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 351, Short.MAX_VALUE)
-                        .addContainerGap()));
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 351, Short.MAX_VALUE)
+                .addContainerGap())
+        );
 
         pack();
         setLocationRelativeTo(null);
-    }// </editor-fold>//GEN-END:initComponents
+    }// Fin Inicializar Componentes
 
-    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenuItem1ActionPerformed
+    private void MI_ActivosActionPerformed(java.awt.event.ActionEvent evt) {
+        // Pendiente
+    }// Mostrar Alumnos Activos
+
+    private void MI_TodosActionPerformed(java.awt.event.ActionEvent evt) {
+        // Pendientewdw
+    }// Mostrar Todos los Alumnos
+
+    private void MI_InsertarActionPerformed(java.awt.event.ActionEvent evt) {
         InsertarA.setVisible(true);
         Modificar.setVisible(false);
         Desactivar.setVisible(false);
         InsertarC.setVisible(false);
-    }// GEN-LAST:event_jMenuItem1ActionPerformed
-
-    private void jMenu3ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenu3ActionPerformed
-
-    }// GEN-LAST:event_jMenu3ActionPerformed
-
-    private void jMenu3MouseClicked(java.awt.event.MouseEvent evt) {// GEN-FIRST:event_jMenu3MouseClicked
-
-    }// GEN-LAST:event_jMenu3MouseClicked
-
-    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenuItem7ActionPerformed
-        this.setVisible(false);
-        LoginUI Login = new LoginUI();
-        Login.setVisible(true);
-    }// GEN-LAST:event_jMenuItem7ActionPerformed
-
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenuItem2ActionPerformed
+    }// Insertar Alumno
+    
+    private void MI_ModificarActionPerformed(java.awt.event.ActionEvent evt) {
         Modificar.setVisible(true);
         InsertarA.setVisible(false);
         Desactivar.setVisible(false);
         InsertarC.setVisible(false);
-    }// GEN-LAST:event_jMenuItem2ActionPerformed
+    }// Modificar Alumno
 
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenuItem3ActionPerformed
+    private void MI_DesactivarActionPerformed(java.awt.event.ActionEvent evt) {
         Desactivar.setVisible(true);
         Modificar.setVisible(false);
         InsertarA.setVisible(false);
         InsertarC.setVisible(false);
-    }// GEN-LAST:event_jMenuItem3ActionPerformed
+    }// Desactivar Alumno
 
-    private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jMenuItem6ActionPerformed
+    private void MI_InsertarCActionPerformed(java.awt.event.ActionEvent evt) {
         InsertarC.setVisible(true);
         Modificar.setVisible(false);
         Desactivar.setVisible(false);
         InsertarA.setVisible(false);
-    }// GEN-LAST:event_jMenuItem6ActionPerformed
-
+    }// Insertar Campus
+    
+   
+    private void MI_CerrarActionPerformed(java.awt.event.ActionEvent evt) {
+        this.setVisible(false);
+        LoginUI Login = new LoginUI();
+        Login.setVisible(true);
+    }// Cerrar Sesion
+    
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-        // (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-         * look and feel. For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
+
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -191,30 +195,28 @@ public class AlumnosUI extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(AlumnosUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        // </editor-fold>
-        // </editor-fold>
 
-        /* Create and display the form */
+        // Mostrar Form
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new AlumnosUI().setVisible(true);
             }
         });
-    }
+    }// Metodo Main
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
+    // Variables 
+    private javax.swing.JMenuBar JMenuBar;
+    private javax.swing.JMenuItem MI_Activos;
+    private javax.swing.JMenuItem MI_Cerrar;
+    private javax.swing.JMenuItem MI_Desactivar;
+    private javax.swing.JMenuItem MI_Insertar;
+    private javax.swing.JMenuItem MI_InsertarC;
+    private javax.swing.JMenuItem MI_Modificar;
+    private javax.swing.JMenuItem MI_Todos;
+    private javax.swing.JMenu MenuAlumnos;
+    private javax.swing.JMenu MenuCampus;
+    private javax.swing.JMenu MenuSalir;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    // End of variables declaration//GEN-END:variables
+    // End of Variables 
 }
